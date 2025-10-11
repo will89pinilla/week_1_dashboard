ee0a4626892028a7ab0b19fd10f080e63596fb97
Rodolfo-bit23 <rodolfomorenom@gmail.com>
2025-10-11 13:33:31 -0500

    Cambio al codigo de will

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ app = dash.Dash(
     __name__,
     meta_tags=[{"name": "viewport", "content": "width=device-width, initial-scale=1"}],
 )
-app.title = "Dashboard energia"
+app.title = "Dashboard energia - Modificación Rodolfo"
 
 server = app.server
 app.config.suppress_callback_exceptions = True
